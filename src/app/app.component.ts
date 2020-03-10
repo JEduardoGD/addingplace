6f34b57e3f986c45029bf298fe6cbe45daf4fcfc
Eduardo Gutiérrez <eduardo_gd@hotmail.com>
2020-03-10 06:27:31 -0600

    adding agm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'LocalizadorParques';
+  latitude: number;
+  longitude: number;
+  zoom: number;
+
+  ngOnInit(): void {
+    this.setCurrentLocation();
+  }
+
+
+  // Get Current Location Coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 15;
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDJmqj8RH5D9YulqT-dGvD8n4gbz0LzxjQ',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
